63d89a58ad9dc782201f5a33d7e1fd62e2f8e610
Jonathan <Alev674642@gmail.com>
2022-05-23 17:20:55 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -1,75 +1,87 @@
-import "./App.css";
-import NavBar from "./components/NavBar";
+import "./App.css"
+import NavBar from "./components/NavBar"
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ListeSorties from "./components/sorties/ListeSorties";
-import SortieProvider from "./components/sorties/SortieProvider";
-import UniqueSortie from "./components/sorties/UniqueSortie";
-import Login from "./components/login/login";
-import RequireAuth from "./components/login/RequireAuth";
-import AuthProvider from "./components/login/AuthProvider";
-import SignIn from "./components/login/SignIn";
-import Welcome from "./components/sorties/Welcome";
-import FormikSortie from "./components/sorties/FormikSortie";
-import VisionneuseSorties from "./components/sorties/VisionneuseEvenements";
-import VisionneuseEvenements from "./components/sorties/VisionneuseEvenements";
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import ListeSorties from "./components/sorties/ListeSorties"
+import SortieProvider from "./components/sorties/SortieProvider"
+import UniqueSortie from "./components/sorties/UniqueSortie"
+import Login from "./components/login/login"
+import RequireAuth from "./components/login/RequireAuth"
+import AuthProvider from "./components/login/AuthProvider"
+import SignIn from "./components/login/SignIn"
+import Welcome from "./components/sorties/Welcome"
+import FormikSortie from "./components/sorties/FormikSortie"
+import VisionneuseEvenements from "./components/sorties/VisionneuseEvenements"
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className='App'>
-        <AuthProvider>
-          <SortieProvider>
-            <NavBar></NavBar>
-            <Routes>
-              <Route exact path='/' element={<Welcome />}></Route>
-              <Route exact path='/login' element={<Login />}></Route>
-              <Route exact path='/signin' element={<SignIn />}></Route>
+	return (
+		<BrowserRouter>
+			<div className="App">
+				<AuthProvider>
+					<SortieProvider>
+						<NavBar></NavBar>
+						<Routes>
+							<Route exact path="/" element={<Welcome />}></Route>
+							<Route
+								exact
+								path="/login"
+								element={<Login />}
+							></Route>
+							<Route
+								exact
+								path="/signin"
+								element={<SignIn />}
+							></Route>
 
-              <Route
-                path='/sortie/:idsortie'
-                element={
-                  <RequireAuth>
-                    <UniqueSortie />
-                  </RequireAuth>
-                }></Route>
-              <Route
-                path='/listeSorties'
-                element={
-                  <RequireAuth>
-                    <ListeSorties />
-                  </RequireAuth>
-                }></Route>
-              <Route
-                exact
-                path='/formulairesortieFormik'
-                element={
-                  <RequireAuth>
-                    <FormikSortie />
-                  </RequireAuth>
-                }></Route>
-              <Route
-                exact
-                path='/formulairesortie/:idsortie'
-                element={
-                  <RequireAuth>
-                    <FormikSortie />
-                  </RequireAuth>
-                }></Route>
-              <Route
-                exact
-                path='/visionneuse'
-                element={
-                  <RequireAuth>
-                    <VisionneuseEvenements />
-                  </RequireAuth>
-                }></Route>
-            </Routes>
-          </SortieProvider>
-        </AuthProvider>
-      </div>
-    </BrowserRouter>
-  );
+							<Route
+								path="/sortie/:idsortie"
+								element={
+									<RequireAuth>
+										<UniqueSortie />
+									</RequireAuth>
+								}
+							></Route>
+							<Route
+								path="/listeSorties"
+								element={
+									<RequireAuth>
+										<ListeSorties />
+									</RequireAuth>
+								}
+							></Route>
+							<Route
+								exact
+								path="/formulairesortieFormik"
+								element={
+									<RequireAuth>
+										<FormikSortie />
+									</RequireAuth>
+								}
+							></Route>
+							<Route
+								exact
+								path="/formulairesortie/:idsortie"
+								element={
+									<RequireAuth>
+										<FormikSortie />
+									</RequireAuth>
+								}
+							></Route>
+							<Route
+								exact
+								path="/visionneuse"
+								element={
+									<RequireAuth>
+										<VisionneuseEvenements />
+									</RequireAuth>
+								}
+							></Route>
+						</Routes>
+					</SortieProvider>
+				</AuthProvider>
+			</div>
+		</BrowserRouter>
+	)
 }
 
-export default App;
+export default App
